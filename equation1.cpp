2441f7ea360e862b24c1c1d027e5669da7d1fc57
Anastasia <a1267695@yandex.ru>
2016-12-26 13:48:02 +0300

    Update equation1.cpp

--- equation1.cpp
+++ equation1.cpp
@@ -1,25 +1,98 @@
 #include <gsl/gsl_errno.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv2.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include "equation.h"
 
-double R(int i){
-    return 0;
+double ff(double t, const double y) //функция из правой части
+{
+  return 0;
 }
 
-
 int
-func (double t, const double u[],double f[],
-      void *params)
+jac (double t, const double y[], double *dfdy, 
+     double dfdt[], void *params)
 {
+  int i, j = 0;
+  
   (void)(t); /* avoid unused parameter warning */
-  for (int i=1;i<=N-1;i++){
-     f[i] = R(i)-A/(h*h)*u[i+1]+2*A/(h*h)*u[i]-A/(h*h)*u[i-1];
+  double A = *(double *)params;
+  gsl_matrix_view dfdy_mat 
+    = gsl_matrix_view_array (dfdy, N, N);
+  gsl_matrix * m = &dfdy_mat.matrix; 
+  
+  for(i; i < N; i++)
+  {
+    for(j; j < N; j++)
+    {
+      if(i == j)
+      {
+	gsl_matrix_set (m, i, j, -2 * A / (h*h));
+      }
+      if(i == j - 1 || i == j + 1)
+      {
+	gsl_matrix_set (m, i, j, A / (h*h));
+      }else
+      {
+	gsl_matrix_set (m, i, j, 0);
+      }
+    }
+  }
+  for(int k = 0; k < N; k++)
+  {
+    dfdt[k] = 0.0; //вторые производные??
   }
   return GSL_SUCCESS;
 }
 
+int
+func (double t, const double y[], double f[], 
+      void *params)
+{
+  (void)(t); /* avoid unused parameter warning */
+  double A = *(double *)params;
+  
+  f[0] = 0; //теплоизоляция
+  for(int i = 1; i < N - 1; i++)
+  {
+    f[i] = A / (h*h) * (y[i + 1] - 2 * y[i] + y[i - 1]) + ff(t, y[i]);
+  }
+  f[N - 1] = 0;
+  return GSL_SUCCESS;
+}
+
+void saveResult(const double x[], const double y[], int k) {
+  pid_t pid;
+  int status;
+  //записываем скрипт для gnuplot в файл
+  FILE *scrpt = fopen("/home/user/Desktop/plot.txt", "w");
+  fprintf(scrpt, "set terminal png\n");
+  fprintf(scrpt, "set output '/home/user/Desktop/heat_plot/%d.png'\n", k);
+  fprintf(scrpt, "set yrange [0:5]\n");
+  fprintf(scrpt, "plot '/home/user/Desktop/result.txt' using 1:2 with lines");
+  fclose(scrpt);
+  FILE *res = fopen("/home/user/Desktop/result.txt", "w");
+  //пишем в файл данные
+  fprintf (res, "#\tx\t\t\tu\n");
+  for(int i = 0; i < N; i++)
+  {
+    fprintf (res, "%.5e\t%.5e\n", x[i], y[i]);
+  }
+  
+  fclose(res);
+  //запускаем gnuplot, он рисует график
+  pid = fork();
+  if(pid > 0) {
+    wait(&status);
+  }else {
+    execl("/usr/bin/gnuplot", "gnuplot", 
+	  "-persist", "/home/user/Desktop/plot.txt", NULL);
+  }
+}
 
 // set y with Cauchy condition
 void Cauchy(double y[]) {
